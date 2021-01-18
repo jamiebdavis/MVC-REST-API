dd88495384e86b4202cf433616e8d329d53f6acf
Jamie Davis <jamie.davis@ao.com>
2021-01-18 14:32:19 +0000

    added mock data

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Commander.Data;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,18 +10,23 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
         //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
         }
 
         //GET api/commands/{id}
         [HttpGet("{id}")]
         public ActionResult<Command> GetCommandById(int id)
         {
-
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
         }
     }
 }
